30bfbe7d62c9543af8f7457bc658a45fc54bea39
Vladimir Kondenko <kondenko2011@gmail.com>
2017-01-04 21:40:36 +0300

    Remove unnecessary app module

--- app/src/main/java/com/kondenko/pocketwaka/App.kt
+++ app/src/main/java/com/kondenko/pocketwaka/App.kt
@@ -2,10 +2,8 @@ package com.kondenko.pocketwaka
 
 import android.app.Application
 import android.content.Context
-import com.kondenko.pocketwaka.Const
 import com.kondenko.pocketwaka.dagger.component.DaggerNetComponent
 import com.kondenko.pocketwaka.dagger.component.NetComponent
-import com.kondenko.pocketwaka.dagger.module.AppModule
 import com.kondenko.pocketwaka.dagger.module.NetModule
 
 class App : Application() {
@@ -23,7 +21,6 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         netComponent = DaggerNetComponent.builder()
-                .appModule(AppModule(this))
                 .netModule(NetModule(Const.BASE_URL))
                 .build()
     }
