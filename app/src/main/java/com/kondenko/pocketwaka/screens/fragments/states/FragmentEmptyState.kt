30bbdd6a9116dd2061d2a3dc6f5ee75ecd8d686e
Vladimir Kondenko <kondenko2011@gmail.com>
2017-03-08 15:48:02 +0300

    Update state illustrations

--- app/src/main/java/com/kondenko/pocketwaka/screens/fragments/states/FragmentEmptyState.kt
+++ app/src/main/java/com/kondenko/pocketwaka/screens/fragments/states/FragmentEmptyState.kt
@@ -15,7 +15,7 @@ class FragmentEmptyState : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater!!.inflate(R.layout.layout_all_state_empty, container, false)
+        val view = inflater!!.inflate(R.layout.layout_stats_state_empty, container, false)
         val updateButton = view.findViewById(R.id.button_error_state_update)
         updateButton.setOnClickListener {
             val uri = Const.URL_PLUGINS
