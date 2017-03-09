fd22d3ace1699e95c2760921d481e33f25839abd
Vladimir Kondenko <kondenko2011@gmail.com>
2017-03-09 12:03:35 +0300

    Move the logout logic from the presenter to the activity

--- app/src/main/java/com/kondenko/pocketwaka/screens/activities/main/MainActivity.kt
+++ app/src/main/java/com/kondenko/pocketwaka/screens/activities/main/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.kondenko.pocketwaka.screens.activities.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.kondenko.pocketwaka.R
+import com.kondenko.pocketwaka.api.oauth.AccessTokenUtils
 import com.kondenko.pocketwaka.events.RefreshEvent
+import com.kondenko.pocketwaka.screens.activities.login.LoginActivity
 import com.kondenko.pocketwaka.screens.fragments.stats.FragmentStatsContainer
 import org.greenrobot.eventbus.EventBus
 
@@ -23,6 +26,19 @@ class MainActivity : AppCompatActivity(), MainActivityView {
         presenter = MainActivityPresenter(this)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_activity_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_logout -> logout()
+            R.id.action_refresh -> EventBus.getDefault().post(RefreshEvent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun setFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         with(transaction) {
@@ -31,17 +47,9 @@ class MainActivity : AppCompatActivity(), MainActivityView {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_activity_menu, menu)
-        return true
+    private fun logout() {
+        finish()
+        startActivity(Intent(this, LoginActivity::class.java))
+        AccessTokenUtils.deleteToken(this)
     }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.action_logout -> presenter.logout(this)
-            R.id.action_refresh -> EventBus.getDefault().postSticky(RefreshEvent)
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
 }
