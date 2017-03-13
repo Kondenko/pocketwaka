56274db611e1008076c9443221e1a23404260ec9
Vladimir Kondenko <kondenko2011@gmail.com>
2017-03-13 20:48:18 +0300

    Add the JvmStatic annotation to functions

--- app/src/main/java/com/kondenko/pocketwaka/utils/Encryptor.kt
+++ app/src/main/java/com/kondenko/pocketwaka/utils/Encryptor.kt
@@ -4,6 +4,11 @@ import android.util.Base64
 import com.kondenko.pocketwaka.api.KeysManager
 
 object Encryptor {
+
+    @JvmStatic
     fun decrypt(value: String) = String(Base64.decode(value, Base64.DEFAULT))
+
+    @JvmStatic
     fun encrypt(value: String) = String(Base64.encode(value.toByteArray(), Base64.DEFAULT))
+
 }
