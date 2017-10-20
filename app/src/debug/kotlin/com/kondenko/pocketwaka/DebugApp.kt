7ab08100c99b57371760cb5c321fdca05d158175
Vladimir <kondenko2011@gmail.com>
2017-10-20 14:45:05 +0300

    Add Stetho

--- app/src/debug/kotlin/com/kondenko/pocketwaka/DebugApp.kt
+++ app/src/debug/kotlin/com/kondenko/pocketwaka/DebugApp.kt
@@ -1,5 +1,6 @@
 package com.kondenko.pocketwaka
 
+import com.facebook.stetho.Stetho
 import timber.log.Timber
 
 open class DebugApp : App() {
@@ -7,6 +8,7 @@ open class DebugApp : App() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        Stetho.initializeWithDefaults(this)
     }
 
 }
