61a58bc78a07ae5dc969cf7fdd9722c3989532c8
Vladimir <kondenko2011@gmail.com>
2017-10-26 18:56:48 +0300

    Clean up and update unit tests

--- app/src/test/kotlin/com/kondenko/pocketwaka/utils/EncryptorTest.kt
+++ app/src/test/kotlin/com/kondenko/pocketwaka/utils/EncryptorTest.kt
@@ -1,26 +1,25 @@
 package com.kondenko.pocketwaka.utils
 
+import com.kondenko.pocketwaka.CustomRobolectricTestRunner
 import com.kondenko.pocketwaka.data.auth.model.AccessToken
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 
-/**
- * Created by Kondenko on 10.10.2017.
- */
-@RunWith(JUnit4::class)
+@RunWith(CustomRobolectricTestRunner::class)
 class EncryptorTest {
 
     private val encryptor = Encryptor()
+
     private val token = AccessToken("a", 1.0, "b", "c", "d", "e", 0f)
     private val encryptedToken = encryptor.encryptToken(token)
     private val decryptedToken = encryptor.decryptToken(encryptedToken)
 
     @Test
-    fun shouldBeEqual() = Assert.assertEquals(token, decryptedToken)
+    fun shouldEncryptCorrectly() = assertEquals(token, decryptedToken)
 
     @Test
-    fun shouldDiffer() = Assert.assertNotEquals(token, encryptedToken)
+    fun shouldDecryptCorrectly() = assertNotEquals(token, encryptedToken)
 
 }
