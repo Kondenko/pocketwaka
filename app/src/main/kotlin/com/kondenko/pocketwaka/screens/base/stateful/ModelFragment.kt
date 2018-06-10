31180fc7dfe704ef171fad15d2a492b33f658aee
Vladimir <kondenko2011@gmail.com>
2018-06-10 14:39:34 +0300

    Move the reactive part to the abstract ModelFragment

--- app/src/main/kotlin/com/kondenko/pocketwaka/screens/base/stateful/ModelFragment.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/screens/base/stateful/ModelFragment.kt
@@ -1,11 +1,37 @@
 package com.kondenko.pocketwaka.screens.base.stateful
 
+import android.os.Bundle
 import android.os.Parcelable
 import android.support.v4.app.Fragment
+import android.view.View
+import com.kondenko.pocketwaka.screens.base.stateful.states.ViewState
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.PublishSubject
 
 abstract class ModelFragment<M : Parcelable> : Fragment() {
 
-    abstract fun subscribeToModelChanges(modelObservable: Observable<M>)
+    private val viewStateSubject = PublishSubject.create<ViewState>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewStateSubject.onNext(ViewState.Visible)
+    }
+
+    override fun onDestroyView() {
+        viewStateSubject.onNext(ViewState.Destroyed)
+        super.onDestroyView()
+    }
+
+    fun subscribeToModelChanges(modelObservable: Observable<M>){
+        viewStateSubject
+                .takeWhile { it === ViewState.Visible }
+                .flatMap { modelObservable }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::onModelChanged)
+    }
+
+    protected abstract fun onModelChanged(model: M)
 
 }
--- app/src/main/kotlin/com/kondenko/pocketwaka/screens/stats/FragmentStatsTab.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/screens/stats/FragmentStatsTab.kt
@@ -30,7 +30,6 @@ class FragmentStatsTab : StatefulFragment<StatsModel>(ModelFragmentStats()), Sta
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         App.statsComponent.inject(this)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
