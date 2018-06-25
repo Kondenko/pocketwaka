bd9aa47c6af43e3e52e57792181ba0f2239c8d9d
Vladimir <kondenko2011@gmail.com>
2018-06-25 18:23:17 +0300

    Don't report crashes in debug mode

--- app/src/main/kotlin/com/kondenko/pocketwaka/App.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/App.kt
@@ -1,7 +1,10 @@
 package com.kondenko.pocketwaka
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.kondenko.pocketwaka.di.modulesList
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
 
@@ -9,6 +12,11 @@ open class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        val crashlytics = with(CrashlyticsCore.Builder()) {
+            disabled(BuildConfig.DEBUG)
+            build()
+        }
+        Fabric.with(this, Crashlytics.Builder().core(crashlytics).build())
         startKoin(this, modulesList(this))
     }
 
