d8671d3a8c2af5332a1e540e2a3ef7ab8e0694ee
Vladimir <kondenko2011@gmail.com>
2018-06-25 18:22:51 +0300

    Add more mocked data

--- app/src/mock/kotlin/com/kondenko/pocketwaka/MockApp.kt
+++ app/src/mock/kotlin/com/kondenko/pocketwaka/MockApp.kt
@@ -4,7 +4,6 @@ import com.kondenko.pocketwaka.di.mockModulesList
 import org.koin.standalone.StandAloneContext
 
 class MockApp : DebugApp() {
-
     override fun onCreate() {
         super.onCreate()
         StandAloneContext.loadKoinModules(mockModulesList(this))
--- app/src/mock/kotlin/com/kondenko/pocketwaka/data/stats/service/MockStatsService.kt
+++ app/src/mock/kotlin/com/kondenko/pocketwaka/data/stats/service/MockStatsService.kt
@@ -6,12 +6,15 @@ import com.kondenko.pocketwaka.data.stats.model.StatsDataWrapper
 import com.kondenko.pocketwaka.getJsonAsset
 import io.reactivex.Single
 import io.reactivex.rxkotlin.toSingle
+import timber.log.Timber
 
 class MockStatsService(val context: Context, val gson: Gson) : StatsService {
 
     override fun getCurrentUserStats(tokenHeaderValue: String, range: String): Single<StatsDataWrapper> {
-        val stats = gson.fromJson<StatsDataWrapper>(getJsonAsset(context, "mocks/stats-full.json"), StatsDataWrapper::class.java)
-        return stats.toSingle() // TODO Stats is null
+        val filename = "mocks/stats-only-time.json"
+        Timber.i("Loading mock data from assets/$range")
+        val stats = gson.fromJson<StatsDataWrapper>(getJsonAsset(context, filename), StatsDataWrapper::class.java)
+        return stats.toSingle()
     }
 
 }
