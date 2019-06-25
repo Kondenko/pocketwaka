d5d9c78ce625122aa87e898841628dea69969ca2
Vladimir Kondenko <kondenko2011@gmail.com>
2019-06-25 21:37:55 +0300

    Remove loading delay and add debounce to states

--- app/src/main/kotlin/com/kondenko/pocketwaka/domain/stats/GetStatsState.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/domain/stats/GetStatsState.kt
@@ -20,25 +20,14 @@ class GetStatsState(
         private val connectivityStatusProvider: ConnectivityStatusProvider
 ) : UseCaseObservable<GetStatsState.Params, StatsState>(schedulers) {
 
-    /*
-        When the user tries to refresh the data it refreshes so quickly
-        that the loading screen doesn't have the time to be shown and hidden again.
-        We add a tiny delay to the loading process so users
-        actually see that the loading happens.
-     */
-    private val delayBetweenSkeletonAndActualUi: Long = 75
+    private val debounceDuration: Long = 50
 
     data class Params(val range: String?, val refreshRateMin: Int, val retryAttempts: Int)
 
     override fun build(params: Params?): Observable<StatsState> {
         if (params?.range == null) return Observable.just(Failure.UnknownRange())
-        val initialDelay = Observable.timer(delayBetweenSkeletonAndActualUi, TimeUnit.MILLISECONDS)
-        val loading = getSkeletonPlaceholderData.build()
-                .map { Loading(skeletonData = it) }
-                .flatMap { state ->
-                    initialDelay.map { state }
-                }
         val connectivityStatus = connectivityStatusProvider.isNetworkAvailable()
+        val loading = getSkeletonPlaceholderData.build().map { Loading(skeletonData = it) }
         val interval = Observable.interval(
                 0,
                 params.refreshRateMin.toLong(),
@@ -53,6 +42,8 @@ class GetStatsState(
                 }
                 .scan(::changeState)
                 .distinctUntilChanged(::equal)
+                .debounce(debounceDuration, TimeUnit.MILLISECONDS, schedulers.uiScheduler)
+                .subscribeOn(schedulers.workerScheduler)
     }
 
     private fun getStats(range: String, retryAttempts: Long, isConnected: Boolean) =
@@ -70,6 +61,7 @@ class GetStatsState(
                         if (isConnected) Failure.Unknown(exception = t)
                         else Failure.NoNetwork(exception = t)
                     }
+                    .subscribeOn(schedulers.workerScheduler)
 
     private fun equal(old: StatsState, new: StatsState) = when {
         old is Failure<StatsModelList> && new is Failure<StatsModelList> -> old.exception == new.exception
