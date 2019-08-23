5bd98abe58b4435e2a76b25dd0e297347766dea2
Vladimir Kondenko <kondenko2011@gmail.com>
2019-08-23 21:20:57 +0300

    Use Path instead of Query in CommitsService

--- app/src/main/kotlin/com/kondenko/pocketwaka/data/commits/service/CommitsService.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/data/commits/service/CommitsService.kt
@@ -5,6 +5,7 @@ import com.kondenko.pocketwaka.data.commits.model.Commits
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 private const val projectPathVariable = "project"
@@ -14,7 +15,7 @@ interface CommitsService {
     @GET("users/current/projects/{$projectPathVariable}/commits")
     fun getCommits(
             @Header(Const.HEADER_BEARER_NAME) tokenHeaderValue: String,
-            @Query(projectPathVariable) project: String? = null,
+            @Path(projectPathVariable) project: String? = null,
             @Query("author") author: String? = null,
             @Query("page") page: Int? = null
     ): Single<Commits>
