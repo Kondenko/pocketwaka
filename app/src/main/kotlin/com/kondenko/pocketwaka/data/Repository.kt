7d9774db59266bc7ca1516cb6ee4f5b4c142baec
Vladimir Kondenko <kondenko2011@gmail.com>
2019-08-23 14:41:38 +0300

    Add doc comments for Repository

--- app/src/main/kotlin/com/kondenko/pocketwaka/data/Repository.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/data/Repository.kt
@@ -7,11 +7,29 @@ import io.reactivex.Single
 import io.reactivex.rxkotlin.subscribeBy
 import timber.log.Timber
 
+/**
+ * This class encapsulates the logic of fetching data from a server, caching it,
+ * and returning data from cache if the server returns an error.
+ *
+ * @param Params parameters to be used for fetching data both from the server and from cache
+ * @param ServerModel the entity returned from the server
+ * @param DbModel the entity to be stored in cache
+ *
+ * @param serverDataProvider a function to fetch data from the server
+ * @param cachedDataProvider a function to fetch data from cache
+ *
+ */
 abstract class Repository<Params, ServerModel, DbModel : CacheableModel<*>>(
         private val serverDataProvider: (Params) -> Single<ServerModel>,
         private val cachedDataProvider: (Params) -> Maybe<DbModel>
 ) {
 
+    /**
+     * First returns data from cache if it's available. Then returns data from server and caches it.
+     *
+     * @param params parameters to fetch data with
+     * @param converter a function to convert [ServerModel] to [DbModel]
+     */
     fun getData(params: Params, converter: (Params, ServerModel) -> DbModel?): Observable<DbModel> {
         val cache = getDataFromCache(params)
         val server = getDataFromServer(params)
@@ -34,6 +52,9 @@ abstract class Repository<Params, ServerModel, DbModel : CacheableModel<*>>(
                 .distinctUntilChanged()
     }
 
+    /**
+     * Fetches data from server without caching.
+     */
     private fun getDataFromServer(params: Params): Observable<ServerModel> =
             serverDataProvider.invoke(params)
                     .toObservable()
@@ -43,8 +64,19 @@ abstract class Repository<Params, ServerModel, DbModel : CacheableModel<*>>(
                     .toObservable()
                     .map { setIsFromCache(it, true) }
 
+    /**
+     * Put [data] into cache
+     *
+     * @return a [Completable] which completes if [data] have been cached successfully
+     */
     protected abstract fun cacheData(data: DbModel): Completable
 
+    /**
+     * Sets the [DbModel::isFromCache] field to specify whether the model is obtained
+     * from the server or from cache.
+     *
+     * @return a copy [DbModel] with the [DbModel::isFromCache] field changed
+     */
     protected abstract fun setIsFromCache(model: DbModel, isFromCache: Boolean): DbModel
 
 }
