35055529cf4c3f79d77b5289464fdf3679781a0e
Vladimir Kondenko <kondenko2011@gmail.com>
2019-09-11 16:41:36 +0300

    Move Timber initialization to App.kt

--- app/src/main/kotlin/com/kondenko/pocketwaka/App.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/App.kt
@@ -8,17 +8,25 @@ import com.kondenko.pocketwaka.di.koinModules
 import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 
 open class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        val isDebug = BuildConfig.DEBUG
+
         val crashlytics = with(CrashlyticsCore.Builder()) {
-            disabled(BuildConfig.DEBUG)
+            disabled(isDebug)
             build()
         }
         Fabric.with(this, Crashlytics.Builder().core(crashlytics).build())
+
+        if (isDebug) {
+            Timber.plant(Timber.DebugTree())
+        }
+
         startKoin {
             logger(TimberLogger())
             androidContext(this@App)
