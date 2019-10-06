310e62bad0e083345c48ec2a385d78afeae63e5b
Vladimir Kondenko <kondenko2011@gmail.com>
2019-10-06 19:07:15 +0300

    Disable Crashlytics for debug builds

--- app/src/main/kotlin/com/kondenko/pocketwaka/App.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/App.kt
@@ -1,11 +1,8 @@
 package com.kondenko.pocketwaka
 
 import android.app.Application
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
 import com.kondenko.pocketwaka.di.TimberLogger
 import com.kondenko.pocketwaka.di.koinModules
-import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
@@ -15,18 +12,7 @@ open class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val isDebug = BuildConfig.DEBUG
-
-        val crashlytics = with(CrashlyticsCore.Builder()) {
-            disabled(isDebug)
-            build()
-        }
-        Fabric.with(this, Crashlytics.Builder().core(crashlytics).build())
-
-        if (isDebug) {
-            Timber.plant(Timber.DebugTree())
-        }
-
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         startKoin {
             logger(TimberLogger())
             androidContext(this@App)
