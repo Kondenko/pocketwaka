8b862e0decd7aa89206d26eafacc760b1f2390f9
Vladimir Kondenko <kondenko2011@gmail.com>
2019-10-18 23:10:42 +0300

    Get rid of animations in RecyclerViewSkeleton
    They were so laggy they actually made UX worse

--- app/src/main/kotlin/com/kondenko/pocketwaka/ui/skeleton/RecyclerViewSkeleton.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/ui/skeleton/RecyclerViewSkeleton.kt
@@ -1,22 +1,12 @@
 package com.kondenko.pocketwaka.ui.skeleton
 
-import android.view.View
-import android.view.ViewPropertyAnimator
-import android.view.animation.AccelerateInterpolator
-import android.view.animation.DecelerateInterpolator
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerViewSkeleton<T, ADAPTER : SkeletonAdapter<T, *>>(
-      adapterCreator: (Boolean) -> ADAPTER,
-      skeletonItems: List<T>,
-      private val crossfadeAnimDuration: Int = 100
-) {
+class RecyclerViewSkeleton<T, ADAPTER : SkeletonAdapter<T, *>>(adapterCreator: (Boolean) -> ADAPTER, skeletonItems: List<T>) {
 
     val actualAdapter: ADAPTER = adapterCreator(false)
 
     val skeletonAdapter: ADAPTER = adapterCreator(true)
-
-    private var animator: ViewPropertyAnimator? = null
 
     init {
         skeletonAdapter.items = skeletonItems
@@ -32,7 +22,7 @@ class RecyclerViewSkeleton<T, ADAPTER : SkeletonAdapter<T, *>>(
     fun show(recyclerView: RecyclerView) {
         recyclerView.adapter.let {
             if (it == null || (it as? SkeletonAdapter<*, *>)?.showSkeleton == false) {
-                setAdapter(recyclerView, skeletonAdapter, it != null)
+                recyclerView.adapter = skeletonAdapter
             }
         }
     }
@@ -40,36 +30,9 @@ class RecyclerViewSkeleton<T, ADAPTER : SkeletonAdapter<T, *>>(
     fun hide(recyclerView: RecyclerView) {
         recyclerView.adapter.let {
             if (it == null || (it as? SkeletonAdapter<*, *>)?.showSkeleton == true) {
-                setAdapter(recyclerView, actualAdapter, it != null)
+                recyclerView.adapter = actualAdapter
             }
         }
-    }
-
-    private fun setAdapter(recyclerView: RecyclerView, adapter: ADAPTER, animate: Boolean) {
-        val setAdapter = { recyclerView.adapter = adapter }
-        if (animate) recyclerView.crossfade(setAdapter)
-        else setAdapter()
-    }
-
-    private inline fun View.crossfade(crossinline doWhenInvisible: () -> Unit) {
-        animator?.cancel()
-        val duration = crossfadeAnimDuration / 2L
-        animator = animate()
-              .alpha(0f)
-              .withLayer()
-              .setDuration(duration)
-              .setInterpolator(DecelerateInterpolator())
-              .withEndAction {
-                  doWhenInvisible()
-                  animate()
-                        .alpha(1f)
-                        .withLayer()
-                        .setDuration(duration)
-                        .setInterpolator(AccelerateInterpolator())
-                        .start()
-              }
-              .apply(ViewPropertyAnimator::start)
-
     }
 
 }
