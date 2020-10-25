dddfc7c111761fe9628a1bd88d4c958f10335bb0
Vladimir Kondenko <kondenko2011@gmaill.com>
2020-10-25 17:53:43 +0300

    Remove temporary code

--- app/src/main/kotlin/com/kondenko/pocketwaka/data/SharedPreferencesRepository.kt
+++ app/src/main/kotlin/com/kondenko/pocketwaka/data/SharedPreferencesRepository.kt
@@ -1,6 +1,7 @@
 package com.kondenko.pocketwaka.data
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 
 class SharedPreferencesRepository(private val sharedPreferences: SharedPreferences) {
 
@@ -8,15 +9,10 @@ class SharedPreferencesRepository(private val sharedPreferences: SharedPreferenc
         private const val KEY_HAS_SEEN_SUMMARY_ONBOARDING = "has_seen_summary_onboarding"
     }
 
-    var hasSeenSummaryOnboarding: Boolean = false
-
-/*
-    STOPSHIP // TODO Uncomment
     var hasSeenSummaryOnboarding: Boolean
         get() = sharedPreferences.getBoolean(KEY_HAS_SEEN_SUMMARY_ONBOARDING, false)
         set(value) = sharedPreferences.edit {
             putBoolean(KEY_HAS_SEEN_SUMMARY_ONBOARDING, value)
         }
-*/
 
 }
